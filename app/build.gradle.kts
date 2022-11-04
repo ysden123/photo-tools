1b2917c3f74be2d90132f7155bc76eb1860508c1
Yuriy Stul <ysden123@gmail.com>
2022-11-04 16:24:51 +0200

    V 2.0 (with UI)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("org.openjfx.javafxplugin") version ("0.0.13")
 }
+
+group = "com.stulsoft"
+version = "2.0.0"
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -9,19 +13,17 @@ repositories {
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-
     // This dependency is used by the application.
     implementation("org.apache.commons:commons-imaging:1.0-alpha3")
 }
 
 application {
+    mainModule.set("com.stulsoft.photo.tools")
     // Define the main class for the application.
     mainClass.set("com.stulsoft.photo.tools.App")
 }
 
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
+javafx {
+    version = "19"
+    modules("javafx.controls", "javafx.fxml")
 }
